7402b83a039a0f578d316427cf2354ec257496ff
Omar Cabaleiro <51969062+OmarCabaleiro@users.noreply.github.com>
2024-06-10 10:33:15 -0400

    Update script.js
    added many more cocktail names

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-var cocktails = ["Amaretto Sour", "Aperol Spritz", "Bay Breeze", "Bellini", "Cosmopolitan", "Dirty Martini", "French 75", "Gimlet", "Godfather", "Long Island Iced Tea", "Madras", "Martini", "Manhattan", "Margarita", "Mimosa", "Negroni", "New York Sour", "Old Fashioned", "Perfect Manhattan", "popular Highballs, general", "Rob Roy (Scotch Manhattan)", "Scotch Sour", "Sea Breeze", "Sex on the Beach", "Side Car", "Tequila Sunrise", "Tom Collins"]
+const cocktails = [
+  "50/50 Martini", "Amaretto Sour", "Aperol Spritz", "Appletini", "Bay Breeze", "Bellini", "Bloody Mary", "Boulevardier (rocks)", "Boulevardier (up)", "Brandy Flip", "Caipirinha", "Cosmopolitan", "Daiquiri", "Dark 'n' Stormy", "Death in the Afternoon (Hemingway Champagne)", "Dirty Martini", "Dry Manhattan", "Dry Martini", "Extra Dry Martini", "French 125", "French 75", "Gibson", "Gimlet", "Gin Sour", "Godfather", "John Collins", "Kamikaze", "Long Island Iced Tea", "Madras", "Mai Tai", "Martini", "Manhattan", "Margarita", "Midori Sour", "Mimosa", "Mojito (fresh)", "Mojito (shortcut)", "Moscow Mule", "Negroni", "New York Sour", "Old Fashioned", "Perfect Manhattan", "Paloma", "Pina Colada", "Pina Colada (shaken, rocks)", "Pisco Sour", "popular Highballs, general", "Rob Roy (Scotch Manhattan)", "Sangria (pitcher)", "Scotch Sour", "Screwdriver", "Sea Breeze", "Sex on the Beach", "Side Car", "Tequila Sunrise", "Tom Collins", "Vesper", "Vodka Collins", "Vodka Sour", "Wet Martini"
+]
 
 // draw 1 random cocktail name from array
 function drawCocktail() {
